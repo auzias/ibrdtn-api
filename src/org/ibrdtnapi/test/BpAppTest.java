06ff5ec982b6b69dfed9f739b5a84604bcb2adf5
auzias <github@auzias.net>
2015-04-12 22:09:43 +0200

    Tests started. Should I do this a TDD project?

---
+++ src/org/ibrdtnapi/test/BpAppTest.java
@@ -0,0 +1,46 @@
+package org.ibrdtnapi.test;
+
+import static org.junit.Assert.*;
+
+import org.ibrdtnapi.BpApplication;
+import org.ibrdtnapi.Bundle;
+import org.junit.Test;
+
+public class BpAppTest {
+	private class BpAppPrintingTest extends BpApplication {
+		@Override
+		protected void bundleReceived(Bundle b) {
+			System.out.println("[BpAppPrintingTest::bundleReceived]" +
+							   "Received bundle:" + b.toString());
+		}
+	}
+
+	@Test
+	public void testSend() {
+		BpAppPrintingTest app = new BpAppPrintingTest();
+		boolean send = app.send(null);
+		if(send) {
+			fail("null-bundle shoud not be send, and the method should return *false*");
+		} else {
+			send = app.send(new Bundle());
+			assertTrue("Bundle not-null sent", send);
+		}
+		
+	}
+
+	@Test
+	public void testGetDispatcher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBundleReceived() {
+		fail("Not yet implemented");
+	}
+
+}
