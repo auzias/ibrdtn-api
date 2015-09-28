31233b8072648980bdbacdf8b1ba0418bd8d0586
auzias <github@auzias.net>
2015-09-28 13:38:51 +0200

    PayloadBlock class to manage payload block as list of a blocks (and not HAVING to use a *single* payload block)

---
+++ src/org/ibrdtnapi/entities/PayloadBlock.java
@@ -0,0 +1,61 @@
+package org.ibrdtnapi.entities;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+@SuppressWarnings("restriction")
+public class PayloadBlock {
+	private static final Logger log = Logger.getLogger(Bundle.class.getName());
+	private String encoded = null;
+	private byte[] decoded = null;
+	
+	public PayloadBlock(String encoded) {
+		this.setEncoded(encoded);
+	}
+
+	public PayloadBlock(byte[] decoded) {
+		this.setDecoded(decoded);
+	}
+
+	public PayloadBlock(PayloadBlock payloadBlock) {
+		this(payloadBlock.getEncoded());
+	}
+
+	public String getEncoded() {
+		return this.encoded;
+	}
+
+	public byte[] getDecoded() {
+		return this.decoded;
+	}
+
+	public int getLength() {
+		return encoded.length();
+	}
+
+	public void setEncoded(String encoded) {
+		this.encoded = new String(encoded);
+		try {
+			this.decoded = new BASE64Decoder().decodeBuffer(this.encoded);
+		} catch (IOException e) {
+			PayloadBlock.log.warning("Could not base64::decode() the payload of the bundle:" + this + ". " + e.getMessage());
+		}
+	}
+
+	private void setDecoded(byte[] decoded) {
+		this.decoded = new byte[decoded.length];
+		System.arraycopy(decoded, 0, this.decoded, 0, decoded.length);
+		this.encoded = new String(new BASE64Encoder().encodeBuffer(this.decoded)).trim();
+	}
+
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append(((this.getLength() == 0) ? "X" : this.getLength()) + "):");
+		str.append("" + this.encoded);
+		str.append("#" + new String(this.decoded) + "#");
+		return str.toString();
+	}
+}
